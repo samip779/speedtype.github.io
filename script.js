cc69d4b238b33630f020b6a339be3d60b2c83970
samip779 <samipluitel28@gmail.com>
2021-07-23 15:15:56 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,42 @@
-const word = document.getElementById('word');
-const text = document.getElementById('text');
-const scoreEl = document.getElementById('score');
-const timeEl = document.getElementById('time');
-const endgameEl = document.getElementById('end-game-container');
-const settingsBtn = document.getElementById('settings-btn');
-const settings = document.getElementById('settings');
-const settingsForm = document.getElementById('settings-form');
-const difficultySelect = document.getElementById('difficulty');
+const word = document.getElementById("word");
+const text = document.getElementById("text");
+const scoreEl = document.getElementById("score");
+const timeEl = document.getElementById("time");
+const endgameEl = document.getElementById("end-game-container");
+const settingsBtn = document.getElementById("settings-btn");
+const settings = document.getElementById("settings");
+const settingsForm = document.getElementById("settings-form");
+const difficultySelect = document.getElementById("difficulty");
 
 // list of words
 const words = [
-  'sigh',
-  'tense',
-  'airplane',
-  'ball',
-  'pies',
-  'juice',
-  'warlike',
-  'bad',
-  'north',
-  'dependent',
-  'steer',
-  'silver',
-  'highfalutin',
-  'superficial',
-  'quince',
-  'eight',
-  'feeble',
-  'admit',
-  'drag',
-  'loving',
-  'agriculture',
-  'infrastructure',
-  'surreal'
+  "sigh",
+  "tense",
+  "airplane",
+  "ball",
+  "pies",
+  "juice",
+  "warlike",
+  "bad",
+  "north",
+  "dependent",
+  "steer",
+  "silver",
+  "highfalutin",
+  "superficial",
+  "quince",
+  "eight",
+  "feeble",
+  "admit",
+  "drag",
+  "loving",
+  "agriculture",
+  "infrastructure",
+  "surreal",
+  "Anachronistic",
+  "Clamor",
+  "callous",
+  "disparate",
 ];
 
 let ramdomWord;
@@ -49,61 +53,59 @@ const timeInterval = setInterval(updateTime, 1000);
 
 // generate ramdom word from array
 function getRandomWord() {
-	return words[Math.floor(Math.random() * words.length)];
+  return words[Math.floor(Math.random() * words.length)];
 }
-
 
 // Add word to dom
 function addWordToDOM() {
-	randomWord = getRandomWord();
-	word.innerHTML = randomWord;
+  randomWord = getRandomWord();
+  word.innerHTML = randomWord;
 }
 
 // update score
 function updateScore() {
-	 score++;
-	 scoreEl.innerHTML = score;
+  score++;
+  scoreEl.innerHTML = score;
 }
 
 // update time
 function updateTime() {
-	time--;
-	timeEl.innerHTML = time + 's';
-	if(time === 0) {
-		clearInterval(timeInterval);
-		// end game 
-		gameOver();
-	}
+  time--;
+  timeEl.innerHTML = time + "s";
+  if (time === 0) {
+    clearInterval(timeInterval);
+    // end game
+    gameOver();
+  }
 }
 
 // game over. show end screen
 function gameOver() {
-	endgameEl.innerHTML = `
+  endgameEl.innerHTML = `
 		<h1>Time ran out </h1>
 		<p>Your final score is ${score}</p>
 		<button onClick="location.reload()">Reload</button>
 	
 	`;
 
-	endgameEl.style.display = 'flex';
+  endgameEl.style.display = "flex";
 }
 
 addWordToDOM();
 
 // Event listners
 
-text.addEventListener('input', e => {
-	const insertedText = e.target.value;
-	if (insertedText === randomWord) {
-		addWordToDOM();
-		updateScore();
+text.addEventListener("input", (e) => {
+  const insertedText = e.target.value;
+  if (insertedText === randomWord) {
+    addWordToDOM();
+    updateScore();
 
-		// clear
-		e.target.value = '';
+    // clear
+    e.target.value = "";
 
-		time += 5;
+    time += 5;
 
-		updateTime();
-	}
+    updateTime();
+  }
 });
-
